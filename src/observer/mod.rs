a024db87cd6efb3bc0bc3f29cd658f97dc289a87
David Zwerdling <zwerdlds@gmail.com>
2023-05-16 00:39:36 -0700

    vector subject name corrected; hashable command added;  command observer added;  hash subject added;

--- src/observer/mod.rs
+++ src/observer/mod.rs
@@ -1,13 +1,46 @@
-use crate::command::Command;
+pub mod command_observer;
+pub mod hash_subject;
+pub mod vector_subject;
 
-pub mod vector_observer;
+/// An object which can be notified.
+pub trait Observer
+{
+    fn notify(&self);
+}
 
-pub trait Observer = Command;
+/// An object which notifies some group of [`Observer`]s.
+pub trait Subject
+{
+    fn notify(&self);
+}
 
-pub trait Observable
+/// Gives the ability for additional [`Observer`]s to be added to the list of
+/// notified objects.
+pub trait Attachable<O>
+where
+    O: Observer,
 {
     fn attach(
         &mut self,
-        observer: dyn Observer,
+        attach_observer: O,
     );
+}
+
+/// An Error which occurs during detachment.
+pub enum ObserverDetachError
+{
+    /// The specified observer couldn't be found.
+    ObserverNotFound,
+}
+
+/// Gives the ability for [`Observer`]s to be removed from the list of notified
+/// objects.
+pub trait Detachable<O>
+where
+    O: Observer,
+{
+    fn detach(
+        &mut self,
+        detach_observer: O,
+    ) -> Result<(), ObserverDetachError>;
 }
---
+++ src/observer/vector_subject.rs
@@ -0,0 +1,210 @@
+use super::{
+    Attachable,
+    Detachable,
+    Observer,
+    ObserverDetachError,
+    Subject,
+};
+
+/// Implements [`Subject`] backed by a [`Vec<T>`].  If `T` implements [`Eq`],
+/// then [`Detachable`] is also implemented.  [`Attachable`] is always
+/// implemented.
+///
+/// # Examples
+/// ```
+/// use roopes_lib::{
+///     command::lambda_command::LambdaCommand,
+///     observer::{
+///         vector_subject::VectorSubject,
+///         Attachable,
+///         Subject,
+///     },
+/// };
+/// use std::{
+///     cell::RefCell,
+///     rc::Rc,
+/// };
+///
+/// let mut vs = VectorSubject::new();
+///
+/// let has_run = Rc::new(RefCell::new(false));
+/// {
+///     let has_run = has_run.clone();
+///
+///     let lc = LambdaCommand::new(move || {
+///         (*has_run.borrow_mut()) = true;
+///     });
+///
+///     vs.attach(lc);
+/// }
+///
+/// assert!(!(*has_run.borrow()));
+/// vs.notify();
+/// assert!((*has_run.borrow()));
+/// ```
+pub struct VectorSubject<O>
+where
+    O: Observer,
+{
+    listeners: Vec<O>,
+}
+
+impl<O> VectorSubject<O>
+where
+    O: Observer,
+{
+    pub fn new() -> VectorSubject<O>
+    {
+        let listeners = Vec::new();
+
+        VectorSubject { listeners }
+    }
+}
+
+impl<O> Subject for VectorSubject<O>
+where
+    O: Observer,
+{
+    fn notify(&self)
+    {
+        self.listeners.iter().for_each(|l| l.notify());
+    }
+}
+
+impl<O> Attachable<O> for VectorSubject<O>
+where
+    O: Observer,
+{
+    fn attach(
+        &mut self,
+        attach_observer: O,
+    )
+    {
+        self.listeners.push(attach_observer);
+    }
+}
+
+impl<O> Detachable<O> for VectorSubject<O>
+where
+    O: Observer + Eq,
+{
+    fn detach(
+        &mut self,
+        detach_observer: O,
+    ) -> Result<(), ObserverDetachError>
+    {
+        let (i, _) = self
+            .listeners
+            .iter()
+            .enumerate()
+            .find(|(_, o)| o.eq(&&detach_observer))
+            .ok_or(ObserverDetachError::ObserverNotFound)?;
+
+        self.listeners.swap_remove(i);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use crate::{
+        command::lambda_command::LambdaCommand,
+        observer::{
+            vector_subject::VectorSubject,
+            Attachable,
+            Subject,
+        },
+    };
+    use std::{
+        cell::RefCell,
+        rc::Rc,
+    };
+
+    #[test]
+    fn simple_vector_subject_notify()
+    {
+        let mut vs = VectorSubject::new();
+
+        let has_run = Rc::new(RefCell::new(false));
+        let has_run_ext = has_run.clone();
+
+        let lc = LambdaCommand::new(move || {
+            (*has_run_ext.borrow_mut()) = true;
+        });
+
+        vs.attach(lc);
+
+        vs.notify();
+
+        assert!((*has_run.borrow()));
+    }
+
+    #[test]
+    fn toggle_vector_subject_notify()
+    {
+        let mut vs = VectorSubject::new();
+
+        let has_run_toggle = Rc::new(RefCell::new(false));
+        let has_run_toggle_ext = has_run_toggle.clone();
+
+        let lc = LambdaCommand::new(move || {
+            let tgl = *has_run_toggle_ext.borrow();
+
+            (*has_run_toggle_ext.borrow_mut()) = !tgl;
+        });
+
+        vs.attach(lc);
+
+        assert!(!(*has_run_toggle.borrow()));
+
+        vs.notify();
+        assert!((*has_run_toggle.borrow()));
+
+        vs.notify();
+        assert!(!(*has_run_toggle.borrow()));
+
+        vs.notify();
+        assert!((*has_run_toggle.borrow()));
+    }
+
+    #[test]
+    fn multiple_vector_subject_notify()
+    {
+        let mut vs = VectorSubject::new();
+
+        let has_run_1 = Rc::new(RefCell::new(false));
+        let has_run_1_ext = has_run_1.clone();
+
+        let lc = LambdaCommand::new(move || {
+            (*has_run_1_ext.borrow_mut()) = true;
+        });
+
+        vs.attach(lc);
+
+        assert!(!(*has_run_1.borrow()));
+
+        vs.notify();
+        assert!((*has_run_1.borrow()));
+
+        let mut vs = VectorSubject::new();
+
+        let has_run_2 = Rc::new(RefCell::new(false));
+        let has_run_2_ext = has_run_2.clone();
+
+        let lc = LambdaCommand::new(move || {
+            (*has_run_2_ext.borrow_mut()) = true;
+        });
+
+        vs.attach(lc);
+
+        assert!((*has_run_1.borrow()));
+        assert!(!(*has_run_2.borrow()));
+
+        vs.notify();
+
+        assert!((*has_run_1.borrow()));
+        assert!((*has_run_2.borrow()));
+    }
+}
