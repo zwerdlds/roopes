e3636f18731bae261a6f7da92798be26cf40e364
David Zwerdling <zwerdlds@gmail.com>
2023-06-16 12:38:58 -0700

    ropes -> roopes file move

---
+++ roopes_examples/structured-logger/logger.rs
@@ -0,0 +1,35 @@
+use crate::{
+    log_formatter::LogFormatter,
+    log_message::LogMessage,
+};
+use roopes::prelude::*;
+use std::rc::Rc;
+
+#[derive(Builder)]
+pub(crate) struct Logger
+{
+    printer: Rc<dyn Handler<String>>,
+    formatter: Rc<dyn LogFormatter>,
+}
+
+impl Logger
+{
+    pub(crate) fn log(
+        &self,
+        message: &LogMessage,
+    )
+    {
+        self.printer.handle(&self.formatter.format_message(message));
+    }
+}
+
+impl Handler<LogMessage> for Logger
+{
+    fn handle(
+        &self,
+        message: &LogMessage,
+    )
+    {
+        self.log(message);
+    }
+}
